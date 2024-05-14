e8df7ead18906c274b799d68804df71c4467bdfe
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-05-14 12:22:27 -0300

    :construction: feat: main armas

--- exercicios/simulador/principal/main.cpp
+++ exercicios/simulador/principal/main.cpp
@@ -10,21 +10,34 @@ using std::string;
 
 int main()
 {
-    ArmaAtaque* arma  = new Rosa("Super Rosa Amarela",0,10);
-    ArmaAtaque* arma2 = new Colher("Colher de Pata",0,50);
-    ArmaDefesa* escudo = new Escudo("Latão", 1);
-    
-    Personagem* p1 = new Clerigo(1, "Clerigo Eq1", 10, arma, escudo);
-    Personagem* p2 = new Cavaleiro(1, "Cavaleiro Eq1", 10, arma, escudo);
-    Personagem* p3 = new Elfo(1, "Elfo Eq1", 10, arma, escudo);
-    Personagem* p4 = new Anao(1, "Anao Eq1", 10, arma, escudo);
-    Personagem* p5 = new Feiticeiro(1, "Feiticeiro Eq1", 10, arma, escudo);
 
-    Personagem* p6 = new Clerigo(1, "Clerigo Eq2", 20, arma, escudo);
-    Personagem* p7 = new Cavaleiro(1, "Cavaleiro Eq2", 20, arma, escudo);
-    Personagem* p8 = new Elfo(1, "Elfo Eq2", 20, arma, escudo);
-    Personagem* p9 = new Anao(1, "Anao Eq2", 20, arma, escudo);
-    Personagem* p10 = new Feiticeiro(1, "Feiticeiro Eq2", 20, arma, escudo);
+    //Armas
+    ArmaAtaque* arma1  = new LivroDivino("Biblia", 0, 12);
+    ArmaAtaque* arma2  = new Espada("Espada Do Dragao", 0, 15);
+    ArmaAtaque* arma3  = new ArcoElfico("Arco Elfico Divino", 0, 8);
+    ArmaAtaque* arma4  = new Machado("Machado Vulcanico", 0, 19);
+    ArmaAtaque* arma5  = new Cajado("Cajado De Esmeraldas", 0, 25);
+
+    //Escudos
+    ArmaDefesa* escudo1 = new AmuletoDivino("Amuleto Das Sombras", 5);
+    ArmaDefesa* escudo2 = new Armadura("Armadura Divina", 5);
+    ArmaDefesa* escudo3 = new CapaDaFurtividade("Capa Da Furtividade Flamejante", 4);
+    ArmaDefesa* escudo4 = new EscudoDeFerro("Escudo De Ferro Dos Anoes", 9);
+    ArmaDefesa* escudo5 = new PergaminhoDeProtecao("Pergaminho De Protecao De Jade", 8);
+    
+    //Personagens Equipe 1
+    Personagem* p1 = new Clerigo(1, "Clerigo Eq1", 30, arma1, escudo1);
+    Personagem* p2 = new Cavaleiro(1, "Cavaleiro Eq1", 10, arma2, escudo2);
+    Personagem* p3 = new Elfo(1, "Elfo Eq1", 10, arma3, escudo3);
+    Personagem* p4 = new Anao(1, "Anao Eq1", 10, arma4, escudo4);
+    Personagem* p5 = new Feiticeiro(1, "Feiticeiro Eq1", 10, arma5, escudo5);
+
+    //Personagens Equipe 1
+    Personagem* p6 = new Clerigo(1, "Clerigo Eq2", 20, arma1, escudo1);
+    Personagem* p7 = new Cavaleiro(1, "Cavaleiro Eq2", 20, arma2, escudo2);
+    Personagem* p8 = new Elfo(1, "Elfo Eq2", 20, arma3, escudo3);
+    Personagem* p9 = new Anao(1, "Anao Eq2", 20, arma4, escudo4);
+    Personagem* p10 = new Feiticeiro(1, "Feiticeiro Eq2", 20, arma5, escudo5);
 
     Simulador* simulador = new Simulador();
     simulador->adicionarPersonagem(p1, 1);
