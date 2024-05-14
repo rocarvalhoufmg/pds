349bc752dc9dccf2c7e5184d878afa9ecd436987
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-05-14 09:35:22 -0300

    :construction: feat: compilador

--- exercicios/simulador/compilar.sh
+++ exercicios/simulador/compilar.sh
@@ -7,3 +7,4 @@ cd core-simulador-cpp
 g++ -c *.cpp 
 cd ..
 g++ principal\\main.cpp acessorios-cpp\\*.o core-simulador-cpp\\*.o
+#g++ -I principal/ -I acessorios-cpp/ -I core-simulador-cpp/ -I personagens-cpp/ principal/*.cpp acessorios-cpp/*.cpp core-simulador-cpp/*.cpp personagens-cpp/*.cpp -o programa
