749301aee0f194ecf35ade2621fbe6c24792e261
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-05-14 09:37:33 -0300

    :fire: remove: aulas

--- aula3/ContaCorrenteOpaca/ContaCorrenteOpaca.h
+++
@@ -1,6 +0,0 @@
-typedef struct ContaCorrenteOpaca ContaCorrenteOpaca;
-
-int haSaldoSuficiente(ContaCorrenteOpaca* conta, double valor);
-ContaCorrenteOpaca* criar_conta(int numero, int cpf);
-void saque_conta(ContaCorrenteOpaca* conta, double valor);
-void deposito_conta(ContaCorrenteOpaca* conta, double valor);
